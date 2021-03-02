fe8d5499c7c4787ca181cd7ca27ba15819a3a1cd
tta9799zzs <tta9799@gmail.com>
2021-03-02 23:58:43 +0700

    Create project - success start here

---
+++ src/test/java/com/hunganh/mymomment/MyMommentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hunganh.mymomment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyMommentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
